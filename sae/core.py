88c90c6598d87b16c93b9fa3769b9955cdd893c4
P1NHE4D <aleks.michael.g2@googlemail.com>
2022-02-22 18:13:33 +0100

    feat: add structural ideas

---
+++ sae/core.py
@@ -0,0 +1,18 @@
+
+class AutoEncoder:
+
+    def __init__(self):
+        # construct model using verification net as a template
+        # use convolutions with stride > 1 for encoder
+        # use transposed convolutions for the decoder
+        # construct two nets: encoder, decoder
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        # pass the given input through the encoder
+        # pass the output of the encoder through the decoder
+        # return the prediction from the decoder
+        pass
