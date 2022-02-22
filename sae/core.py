98ee48bde6ecc607ea39bec7daa623559bab4a96
P1NHE4D <aleks.michael.g2@googlemail.com>
2022-02-22 22:44:33 +0100

    optimize classifier

--- sae/core.py
+++ sae/core.py
@@ -1,3 +1,23 @@
+from keras.models import Sequential
+from keras.layers import Dense, ReLU, Conv2D, Conv2DTranspose, MaxPooling2D, Dropout, Flatten
+
+
+def init_model():
+    model = Sequential()
+
+    # TODO: find a good architecture
+    # encoder
+    # input: images of size 28 x 28 x 3
+    # reduce dimensions, e.g. to 4
+
+    # TODO: how to use transposed 2d convolutions
+    # TODO: find out which scaling method to use
+    # decoder
+    # input: depends on dimensionality reduction of encoder
+    # output: 28 x 28 x 3 images
+
+    return model
+
 
 class AutoEncoder:
 
@@ -5,8 +25,8 @@ class AutoEncoder:
         # construct model using verification net as a template
         # use convolutions with stride > 1 for encoder
         # use transposed convolutions for the decoder
-        # construct two nets: encoder, decoder
-        pass
+        # construct single net, i.e. input -> dim reduction (encoding) -> dim inflation (decoding)
+        self.model = init_model()
 
     def train(self):
         # binary reconstruction loss for decoder
