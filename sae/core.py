0dea0495fddc872dcadbad98fbdc00d0aec41f23
P1NHE4D <aleks.michael.g2@googlemail.com>
2022-02-24 16:01:45 +0100

    add conv model

--- sae/core.py
+++ sae/core.py
@@ -1,6 +1,23 @@
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Reshape
+from keras.layers import Dense, Flatten, Reshape, Conv2D, Conv2DTranspose, MaxPooling2D
 from tensorflow import keras
+
+
+def init_conv_model(learning_rate=0.01):
+    model = Sequential()
+    model.add(Conv2D(32, input_shape=(28, 28, 1), kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu"))
+    model.add(Conv2D(64, input_shape=(28, 28, 32), kernel_size=(3, 3), strides=(2, 2), padding="same", activation="relu"))
+    model.add(Conv2D(64, input_shape=(14, 14, 64), kernel_size=(3, 3), strides=(2, 2), padding="same", activation="relu"))
+    model.add(Conv2D(2, input_shape=(7, 7, 64), kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu"))
+
+    model.add(Conv2DTranspose(64, input_shape=(7, 7, 2), kernel_size=(2, 2), strides=(2, 2), padding="same", activation="relu"))
+    model.add(Conv2DTranspose(32, input_shape=(14, 14, 64), kernel_size=(2, 2), strides=(2, 2), padding="same", activation="relu"))
+    model.add(Conv2D(1, kernel_size=(3, 3), input_shape=(28, 28, 32), strides=(1, 1), padding="same", activation="sigmoid"))
+    model.compile(
+        loss=keras.losses.binary_crossentropy,
+        optimizer=keras.optimizers.Adam(learning_rate=learning_rate)
+    )
+    return model
 
 
 def init_model(learning_rate=0.01):
@@ -12,7 +29,7 @@ def init_model(learning_rate=0.01):
     model.add(Dense(128, activation="relu"))
     model.add(Dense(256, activation="relu"))
     model.add(Dense(784, activation="sigmoid"))
-    model.add(Reshape((28, 28, 1), input_shape=(784, )))
+    model.add(Reshape((28, 28, 1), input_shape=(784,)))
     model.compile(
         loss=keras.losses.binary_crossentropy,
         optimizer=keras.optimizers.Adam(learning_rate=learning_rate)
@@ -23,11 +40,28 @@ def init_model(learning_rate=0.01):
 
 class AutoEncoder:
 
-    def __init__(self):
-        self.model = init_model(0.01)
+    def __init__(self, model_type="nn", learning_rate=0.01, force_retrain=False):
+        self.learning_rate = learning_rate
+        self.force_retrain = force_retrain
+        self.file_path = "./sae/models/sae_model"
+        if model_type == "conv":
+            self.model = init_conv_model(learning_rate)
+        else:
+            self.model = init_model(0.01)
+
+    def load_weights(self):
+        try:
+            print("Weights found. Loading...")
+            self.model.load_weights(filepath=self.file_path)
+            return True
+        except:
+            print("Weights not found. Retraining...")
+            return False
 
     def train(self, x_train, y_train, x_val, y_val, epochs=5):
-        self.model.fit(x=x_train, y=y_train, batch_size=1024, epochs=epochs, validation_data=(x_val, y_val))
+        if not self.load_weights() or self.force_retrain:
+            self.model.fit(x=x_train, y=y_train, batch_size=1024, epochs=epochs, validation_data=(x_val, y_val))
+            self.model.save_weights(filepath=self.file_path)
 
     def predict(self, x):
         return self.model.predict(x)
