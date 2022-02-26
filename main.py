01622aa565c56c2c9f165ef38fb7bc037e447790
P1NHE4D <aleks.michael.g2@googlemail.com>
2022-02-26 17:46:33 +0100

    refactor auto encoder

--- main.py
+++ main.py
@@ -8,16 +8,12 @@ from sae.core import AutoEncoder
 
 
 def main():
-    generator = StackedMNISTData(mode=DataMode.MONO_BINARY_COMPLETE, default_batch_size=2048)
+    generator = StackedMNISTData(mode=DataMode.COLOR_BINARY_COMPLETE, default_batch_size=2048)
 
     x_train, _ = generator.get_full_data_set(training=True)
     x_test, _ = generator.get_full_data_set(training=False)
 
-    # "Translate": Only look at "red" channel; only use the last digit. Use one-hot for labels during training
-    x_train = x_train[:, :, :, [0]]
-    x_test = x_test[:, :, :, [0]]
-
-    ae = AutoEncoder()
+    ae = AutoEncoder(retrain=True, cnn=True)
     ae.compile(
         loss=keras.losses.binary_crossentropy,
         optimizer=keras.optimizers.Adam(learning_rate=0.01)
@@ -30,14 +26,31 @@ def main():
         validation_data=(x_test, x_test)
     )
 
-    idx = np.random.choice(x_test.shape[0], 5)
+    idx = np.random.choice(x_test.shape[0], 1)
     p = x_test[idx]
     imgs = ae.predict(p)
     for i, img in enumerate(imgs):
-        plt.imshow(p[i], cmap='gray')
+        img *= 255
+        plt.imshow(p[i] * 255)
         plt.show()
-        plt.imshow(img, cmap='gray')
+        plt.imshow(img)
         plt.show()
+
+    # generative model
+    rand_encoding = np.random.randn(16, 7, 7)
+    decoding = ae.decoder.predict(rand_encoding)
+
+    f, axs = plt.subplots(4, 4)
+    row = 0
+    col = 0
+    for img in decoding:
+        axs[row, col].set_axis_off()
+        axs[row, col].imshow(img, cmap='gray')
+        col = (col + 1) % 4
+        if col == 0:
+            row += 1
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.show()
 
 
 if __name__ == '__main__':
--- sae/core.py
+++ sae/core.py
@@ -1,25 +1,52 @@
 from keras.models import Sequential, Model
-from keras.layers import Dense, Flatten, Reshape
+from keras.layers import Dense, Flatten, Reshape, Conv2D, UpSampling2D
 
 
-class AutoEncoder(Model):
-
-    def __init__(self, file_path="sae/models/sae_model", retrain=False, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.retrain = retrain
-        self.file_path = file_path
-        self.encoder = Sequential([
+def construct_encoder(cnn):
+    if cnn:
+        encoder = Sequential([
+            Conv2D(16, input_shape=(28, 28, 1), kernel_size=(3, 3), strides=(2, 2), padding="same", activation="relu"),
+            Conv2D(32, kernel_size=(3, 3), strides=(2, 2), padding="same", activation="relu"),
+            Conv2D(1, kernel_size=(3, 3), strides=(1, 1), padding="same"),
+        ])
+    else:
+        encoder = Sequential([
             Flatten(),
             Dense(256, activation="relu"),
             Dense(128, activation="relu"),
             Dense(32),
         ])
-        self.decoder = Sequential([
+    return encoder
+
+
+def construct_decoder(cnn):
+    if cnn:
+        decoder = Sequential([
+            Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu"),
+            UpSampling2D(),
+            Conv2D(16, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu"),
+            UpSampling2D(),
+            Conv2D(1, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="sigmoid"),
+        ])
+    else:
+        decoder = Sequential([
             Dense(128, activation="relu"),
             Dense(256, activation="relu"),
             Dense(784, activation="sigmoid"),
             Reshape((28, 28, 1), input_shape=(784,)),
         ])
+    return decoder
+
+
+class AutoEncoder(Model):
+
+    def __init__(self, file_path="sae/models/sae_model", retrain=False, cnn=True, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.retrain = retrain
+        self.file_path = file_path
+        self.cnn = cnn
+        self.encoder = construct_encoder(self.cnn)
+        self.decoder = construct_decoder(self.cnn)
 
     def call(self, inputs, **kwargs):
         encoded = self.encoder(inputs)
@@ -27,7 +54,7 @@ class AutoEncoder(Model):
         return decoded
 
     # noinspection PyBroadException
-    def fit(self, **kwargs):
+    def fit(self, x, y, validation_data, **kwargs):
         try:
             self.load_weights(filepath=self.file_path)
             weights_loaded = True
@@ -35,9 +62,26 @@ class AutoEncoder(Model):
             print("Weights not found. Retraining...")
             weights_loaded = False
         if not weights_loaded or self.retrain:
-            super().fit(**kwargs)
+            channels = x.shape[3]
+            if channels > 1:
+                x = x[:, :, :, [0]]
+                y = y[:, :, :, [0]]
+                validation_data = (validation_data[0][:, :, :, [0]], validation_data[1][:, :, :, [0]])
+            super().fit(x=x, y=y, validation_data=validation_data, **kwargs)
             print("Storing learned weights...")
             self.save_weights(self.file_path)
+
+    def predict(self, x, **kwargs):
+        x_count = x.shape[0]
+        channels = x.shape[3]
+        reshaped = False
+        if channels > 1:
+            reshaped = True
+            x = x.T.reshape((x_count * channels, x.shape[1], x.shape[2], 1))
+        pred = super().predict(x, **kwargs)
+        if reshaped:
+            pred = pred.T.reshape((x_count, pred.shape[1], pred.shape[2], channels))
+        return pred
 
     def get_config(self):
         super().get_config()
